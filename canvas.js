a491bcc5a9646666e715189db6f073be82156828
paolo.deidda <paolo.deidda@delvi.tech>
2023-12-07 16:18:27 +0100

    initial set-up

---
+++ canvas.js
@@ -0,0 +1,17 @@
+let myCanvas; // Declare a variable to hold the canvas
+let canvasWidth = 400; // Initial canvas width
+let canvasHeight = 400; // Initial canvas
+document.getElementById('canvas').style.width = canvasWidth + 'px';
+document.getElementById('canvas').style.height = canvasHeight + 'px';
+
+function setup() {
+    // Specify the parent container for the canvas using the parent() function
+    myCanvas = createCanvas(canvasWidth, canvasHeight).parent('canvas');
+}
+
+function draw() {
+    background(0); // Set the background color
+
+    // Draw an ellipse at the center of the canvas
+    ellipse(width / 2, height / 2, 50, 50);
+}
