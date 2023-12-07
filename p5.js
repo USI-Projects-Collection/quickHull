c7a2b5f78d9ad249008f1ca6c388fbf38c577ad8
paolo.deidda <paolo.deidda@delvi.tech>
2023-12-07 15:18:33 +0100

    first commit

---
+++ p5.js
@@ -0,0 +1,13 @@
+
+
+
+function setup() {
+    createCanvas(400, 400); // Create a canvas with width and height
+}
+
+function draw() {
+    background(220); // Set the background color
+
+    // Draw an ellipse at the center of the canvas
+    ellipse(width / 2, height / 2, 50, 50);
+}
