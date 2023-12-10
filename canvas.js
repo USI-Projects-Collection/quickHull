1f1582e52f96f49b53a40cf504fdefc164492d62
poli <paolo.deidda@usi.ch>
2023-12-10 11:59:27 +0100

    BEFORE screenshots

--- canvas.js
+++ canvas.js
@@ -1,17 +1,104 @@
+
 let myCanvas; // Declare a variable to hold the canvas
-let canvasWidth = 400; // Initial canvas width
-let canvasHeight = 400; // Initial canvas
-document.getElementById('canvas').style.width = canvasWidth + 'px';
-document.getElementById('canvas').style.height = canvasHeight + 'px';
+let canvasWidth = 600; // Initial canvas width
+let canvasHeight = 800; // Initial canvas height
+
+let nPoints = 10; // Number of points
+let unsortedPoints; // Array to hold the set of points
+let points; // Array to hold the sorted white points
+
+let animationStep; // The animation state index
+let currentState; // The current animation state index
+let endState = false; // Boolean to check if the animation is finished
 
 function setup() {
     // Specify the parent container for the canvas using the parent() function
     myCanvas = createCanvas(canvasWidth, canvasHeight).parent('canvas');
+
+    // the animation start displaing the set points
+    animationStep = 1;
+
+    // Generate the set of points once in the setup
+    unsortedPoints = generateSetOfPoints(nPoints);
+
+    // Sort the points by x-coordinate
+    points = sortPoints([...unsortedPoints]);
+}
+
+function restore() {
+    background(0); // Clear the canvas
+    currentState = animationStep; // Save the current animation state
+    document.getElementById('step').innerHTML = animationStep;
+    for (let i = 0; i < points.length; i++) {
+        points[i].color = colors.WHITE; // Reset the color of all points
+    }
 }
 
 function draw() {
-    background(0); // Set the background color
+    restore();
+    
+    // STEP 1: POINTS GENERATION //
+    if (animationStep) {
+        drawPoints(unsortedPoints, true);
+        animationStep -= 1; 
+    }
 
-    // Draw an ellipse at the center of the canvas
-    ellipse(canvasWidth / 2, canvasHeight / 2, 50, 50);
+    // STEP 2: POINTS SORTING //
+    if (animationStep) {
+        background(0);
+        drawPoints(points, true);
+        animationStep -= 1;
+    }
+
+    // STEP 3: FIRST POINT //
+    let firstPoint = points[0];
+    if (animationStep) {
+        firstPoint.color = colors.RED; // First point in red
+        drawPoint(firstPoint);
+        drawCircle(firstPoint);    
+        animationStep -= 1;
+    }
+
+    // STEP 4: LAST POINT //
+    let lastPoint = points[points.length - 1];
+    if (animationStep) {
+        lastPoint.color = colors.RED; // Last point in red
+        drawPoint(lastPoint);
+        drawCircle(lastPoint);        
+        animationStep -= 1;
+    }
+
+    // STEP 5: FIRST LINE //
+    if (animationStep) {
+        background(0);
+        drawPoints(points);
+        drawLine(firstPoint, lastPoint);
+        animationStep -= 1;
+    }
+
+    // STEP 6: LEFT POINTS //
+    let leftPoints = points.slice(1, points.length - 1);
+    if (animationStep) {
+        background(0);
+        drawPoints(points);
+        drawLine(firstPoint, lastPoint);
+        drawPoints(leftPoints);
+        animationStep -= 1;
+    }
+    
+
+    animationStep = currentState; // Restore the current animation index
+}
+
+// function to control the animation state
+function keyPressed() {
+    if (keyCode === 39) { // Check if the key pressed is the spacebar
+        animationStep++; // Move to the next point
+    } else if (keyCode === 37 && animationStep > 0) {
+        if (animationStep > 1) {
+            animationStep--; // Move to the previous point
+        }
+    } else if (keyCode === 32) {
+        setup(); // Reset the animation
+    }
 }
---
+++ utilities.js
@@ -0,0 +1,66 @@
+
+// enumerator for colors
+const colors = {
+    RED : [255, 0, 0],
+    GREEN : [0, 255, 0],
+    BLUE : [0, 0, 255],
+    YELLOW : [255, 255, 0],
+    MAGENTA : [255, 0, 255],
+    CYAN : [0, 255, 255],
+    WHITE : [255, 255, 255],
+}
+
+// Function to generate an array of white points
+function generateSetOfPoints(n) {
+    let points = [];
+    for (let i = 0; i < n; i++) {
+        let x = random(50, width - 50);
+        let y = random(50, height - 50);
+        newPoint = createVector(x, y);
+        newPoint.color = colors.WHITE;
+        points.push(newPoint);
+    }
+    return points;
+}
+
+function drawPoint(_point) {
+    stroke(_point.color);
+    strokeWeight(4);
+    point(_point);
+}
+
+function drawCircle(_point, color = colors.RED) {
+    noFill();
+    stroke(color);
+    strokeWeight(2);
+    ellipse(_point.x, _point.y, 15, 15); // Draw a circle around the first point
+}
+
+function drawLine(_point1, _point2, color = colors.RED) {
+    stroke(color);
+    strokeWeight(2);
+    line(_point1.x, _point1.y, _point2.x, _point2.y);
+}
+
+// Function to sort points by x-coordinate
+function sortPoints(points) {
+    points.sort(function(a, b) {
+        return a.x - b.x;
+    });
+    return points;
+}
+
+function drawPoints(points, numbers = false) {
+    for (let i = 0; i < points.length; i++) {
+        drawPoint(points[i]);
+        
+        if (numbers) {
+            // Draw the label
+            fill(255);
+            noStroke();
+            textSize(12);
+            textAlign(CENTER, BOTTOM);
+            text(i + 1, points[i].x, points[i].y - 8); // Adjust the value -8 to position the label relative to the point
+        }
+    }
+}
